d7201d4fa853a0e14167c281a300807564658d30
Joss <dev@example.com>
2025-11-22 23:09:22 +0100

    rl

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://easyclub.app',
+  siteUrl: 'https://easyclub.fr',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ['/api/**'],
--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { buttonVariants } from '@/components/ui/button';
 
 export const runtime = 'edge';
-export const dynamic = 'force-dynamic';
 
 export default function NotFound(): React.JSX.Element {
   return (
